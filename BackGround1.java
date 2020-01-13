88b3e4f7ac0e2b91c521e38482a1838458ea333a
Mohammad A Shazad <43075432+Mohammad-A-Shazad@users.noreply.github.com>
2020-01-13 14:06:48 -0500

    Add files via upload

--- BackGround1.java
+++ BackGround1.java
@@ -1,15 +1,14 @@
 import greenfoot.*;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 /**
  * Write a description of class BackGround1 here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-
-import java.awt.Dimension;
-import java.awt.Toolkit;
-
 public class BackGround1 extends World
 {
 
@@ -17,33 +16,49 @@ public class BackGround1 extends World
      * Constructor for objects of class BackGround1.
      * 
      */
+    private static final int w = (int)(Toolkit.getDefaultToolkit().getScreenSize().getWidth()*0.80);
+    private static final int h = (int)(Toolkit.getDefaultToolkit().getScreenSize().getHeight()*0.80);
     
     public BackGround1()
     {    
         //super(1500, 1000, 1);
+        super(w,h,1);
+        final int floorHeight = (int)(h*0.05);
+        final int floorGap = (int)h/4;
+        final int marioHeight = 54;
         
-        super((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(),(int)Toolkit.getDefaultToolkit().getScreenSize().getHeight(),1);
-        int w = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-        int h = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-        addObject(new DK(), 200, 150);
-        addObject(new Floor(), 125, 219);
-        addObject(new Floor(), 375, 219);
-        addObject(new Floor(), 625, 219);
-        addObject(new Floor(), 875, 219);
-        addObject(new Floor2(), 625, 500);
-        addObject(new Floor2(), 875, 500);
-        addObject(new Floor2(), 1125, 500);
-        addObject(new Floor2(), 1375, 500);
-        addObject(new Floor(), 125, 781);
-        addObject(new Floor(), 375, 781);
-        addObject(new Floor(), 625, 781);
-        addObject(new Floor(), 875, 781);
-        addObject(new Floor2(), 125, 1000);
-        addObject(new Floor2(), 375, 1000);
-        addObject(new Floor2(), 625, 1000);
-        addObject(new Floor2(), 875, 1000);
-        addObject(new Floor2(), 1125, 1000);
-        addObject(new Floor2(), 1375, 1000);
-        addObject(new Mario(), 125, 940);
+        
+        addObject(new DK(140, 110), 190, 112);
+        addObject(new BarrelBG(100, 80), 70, 122);
+        //addObject(new DK(), 220, 150);
+        //addObject(new Floor(), 125, 219);
+        //addObject(new Floor(), 375, 219);
+        //addObject(new Floor(), 625, 219);
+        //addObject(new Floor(), 875, 219);
+        //addObject(new Floor2(), 625, 500);
+        //addObject(new Floor2(), 875, 500);
+        //addObject(new Floor2(), 1125, 500);
+        //addObject(new Floor2(), 1375, 500);
+        //addObject(new Floor(), 125, 781);
+        //addObject(new Floor(), 375, 781);
+        //addObject(new Floor(), 625, 781);
+        //addObject(new Floor(), 875, 781);
+        //addObject(new Floor2(), 125, 1000);
+        //addObject(new Floor2(), 375, 1000);
+        //addObject(new Floor2(), 625, 1000);
+        //addObject(new Floor2(), 875, 1000);
+        //addObject(new Floor2(), 1125, 1000);
+        //addObject(new Floor2(), 1375, 1000);
+        addObject(new Floor((int)(w*0.75), floorHeight), (int)((w*0.75)*0.5), h-(floorGap*3));
+        addObject(new Floor2((int)(w*0.75), floorHeight), w-(int)((w*0.75)*0.5), h-(floorGap*2));
+        addObject(new Floor((int)(w*0.75), floorHeight), (int)((w*0.75)*0.5), h-floorGap);
+        addObject(new Floor2(w, floorHeight), (int)(w/2), h);
+        //(int)(h*0.375-(floorHeight/2)-marioHeight+1)
+        addObject(new Ladder((int)(w*0.04), (int)(floorGap-marioHeight+5)), (int)(w*0.33), (int)(h-(floorGap*3)+((floorGap-marioHeight-floorHeight)/2)));
+        addObject(new Ladder((int)(w*0.04), (int)(floorGap-marioHeight+5)), (int)(w*0.66), (int)(h-(floorGap*2)+((floorGap-marioHeight-floorHeight)/2)));
+        
+        addObject(new Mario(36, 54), 125, h-46);
+        
+        addObject(new FireBarrel(36, 54), 60, h-46);
     }
 }
--- Mario.java
+++ Mario.java
@@ -2,83 +2,154 @@ import greenfoot.*;
 public class Mario extends Actor
 {
     int speed;
-    String Marioimage = "mariopixelCopy.png";
+    GreenfootImage marioFacingRight = new GreenfootImage("Mario Facing Right.png");
+    GreenfootImage marioFacingLeft = new GreenfootImage("Mario Facing Left.png");
+    GreenfootImage marioRunningRightHalf = new GreenfootImage("Mario Running Right Half.png");
+    GreenfootImage marioRunningRight = new GreenfootImage("Mario Running Right.png");
+    GreenfootImage marioRunningLeftHalf = new GreenfootImage("Mario Running Left Half.png");
+    GreenfootImage marioRunningLeft = new GreenfootImage("Mario Running Left.png");
+    GreenfootImage marioUnderLadder = new GreenfootImage("Mario Standing (Bottom Ladder).png");
+    GreenfootImage marioClimbingLadder1 = new GreenfootImage("Mario Climbing 1.png");
+    GreenfootImage marioClimbingLadder2= new GreenfootImage("Mario Climbing 2.png");
     long lastTime;
-    int Lives = 3;
-    public void act() 
-    {
-        speed = speed + 1;
+    int lives = 10, animationCounter = 0, marioFrame = 1;
+    Heart[] hearts = new Heart[lives];
+    public Mario(int w, int h){
+        //w=50, h=75
+        marioFacingRight = getImage();
+        marioFacingRight.scale(w,h);
+        marioFacingLeft.scale(w,h);
+        marioRunningRightHalf.scale(w+10,h);
+        marioRunningLeftHalf.scale(w+10,h);
+        marioRunningRight.scale(w+10,h);
+        marioRunningLeft.scale(w+10,h);
+        marioUnderLadder.scale(w,h);
+        marioClimbingLadder1.scale(w+10,h);
+        marioClimbingLadder2.scale(w+10,h);
+    }
+    public void addedToWorld(World world) {
+        if (getWorld() instanceof BackGround1) {
+            for (int i = 0; i < hearts.length; i++) {
+                hearts[i] = new Heart(30, 30);
+                getWorld().addObject(hearts[i], i*50+25, 25);
+            }
+        }
+    }
+    public void animation() {
+        if(Greenfoot.isKeyDown("left")) {
+          if(marioFrame == 1){
+            setImage(marioRunningLeftHalf);
+            marioFrame = 2;
+          }
+          else if(marioFrame == 2){
+            setImage(marioRunningLeft);
+            marioFrame = 1;
+          }  
+        } else if(Greenfoot.isKeyDown("right")){
+            if(marioFrame == 1){
+                setImage(marioRunningRightHalf);
+                marioFrame = 2;
+            }
+            else if(marioFrame == 2){
+              setImage(marioRunningRight);
+              marioFrame = 1;
+            }
+        } 
+        else if(isTouching(Ladder.class) && Greenfoot.isKeyDown("up")){
+                if(marioFrame == 1){
+                    setImage(marioClimbingLadder1);
+                    marioFrame = 2;
+        }       else if(marioFrame == 2){
+                    setImage(marioClimbingLadder2);
+                    marioFrame = 1;
+        }
+        } 
+        else if(isTouching(Ladder.class) && Greenfoot.isKeyDown("down")){
+                if(marioFrame == 1){
+                    setImage(marioClimbingLadder1);
+                    marioFrame = 2;
+        }       else if(marioFrame == 2){
+                    setImage(marioClimbingLadder2);
+                    marioFrame = 1;
+        }
+        } else {
+            setImage(marioFacingRight);
+        }
+        
+}
+    public void act() {
+        speed += 1;
         setLocation( getX(), getY() + speed);
-        getWorld().showText("Lives : "+ Lives +"",1450, 50);
+        //getWorld().showText("Lives : "+ lives +"", 50, 15);
+        animationCounter = animationCounter + 1;
+        if(animationCounter % 10 == 0)
+        {
+            animation();
+        }
         if(isTouching(Barrel.class))
         {
             removeTouching(Barrel.class);
-            Lives = Lives - 1;
+            getWorld().removeObject(hearts[lives-1]);
+            lives--;
         }
-        if(Lives == 0)
+        if(lives == 0)
         {
             getWorld().showText("GAME OVER", 750, 600);
             Greenfoot.stop();
         }
         if(speed > 0)
         {
-            while(isTouching(Floor.class))
-            {
+            if(isTouching(Ladder.class)) {
                 speed = 0;
                 setLocation(getX(), getY() - 1);
-                if(Greenfoot.isKeyDown("up"))
+                //if(Greenfoot.isKeyDown("up")) {
+                //    speed = -5;
+                //}
+            } else {
+                while(isTouching(Floor.class))
                 {
-                    speed = - 27;
+                    speed = 0;
+                    setLocation(getX(), getY() - 1);
+                    if(Greenfoot.isKeyDown("up") && !isTouching(Ladder.class))
+                    {
+                        speed = - 22;
+                    }
                 }
             }
         }
         if(speed <= 0)
         {
-            while(isTouching(Floor.class))
-            {
+            if(isTouching(Ladder.class)) {
                 speed = 0;
-                setLocation(getX(), getY() + 1);
-            }
-        }    
-        if(Greenfoot.isKeyDown("left"))
-        {
-            move(-5);
-            /*if(System.currentTimeMillis() - lastTime > 500 && Marioimage.equals("mariopixelCopy.png"))
-            {
-                Marioimage = "marioleft.png";
-                setImage("marioleft.png");
-                lastTime = System.currentTimeMillis();
-            } else {
-                if(System.currentTimeMillis() - lastTime > 500)
-                {
-                    Marioimage = "mariopixelCopy.png";
-                    setImage("mariopixelCopy.png");
-                    lastTime = System.currentTimeMillis();
+                //setLocation(getX(), getY() - 1);
+                if(Greenfoot.isKeyDown("up")) {
+                    speed = -2;
                 }
-            } */
-            
-           
-            setImage("mariopixelCopy.png");
-            while(isTouching(Floor.class))
-            {
-               move(1);
-            } 
-        } else {
-            if(Greenfoot.isKeyDown("right"))
-            {
-               move(5);
-               setImage("mariopixel.png");
+            } else {
                 while(isTouching(Floor.class))
                 {
-                  move(-1);
-               }
-            } else{
-                setImage("mario-big.png");
+                    speed = 0;
+                    setLocation(getX(), getY() + 1);
+                }
             }
         }
-        if(Greenfoot.isKeyDown("down"))
-        {
-            speed = 50;
+        if(Greenfoot.isKeyDown("right")) {
+            move(1);
+        } else if(Greenfoot.isKeyDown("left")) {
+            move(-1);
         }
-    } 
+        if(isTouching(Ladder.class)) {
+            setLocation(getX(), getY() + 1);
+            if(Greenfoot.isKeyDown("up")) {
+                speed -= 1;
+            } else if(Greenfoot.isKeyDown("down")) {
+                speed += 1;
+            }
+        }
+        // MAY USE THIS FOR POWERUPS DOWN THE LINE
+        //if(Greenfoot.isKeyDown("l")) {
+        //    lives++;
+        //    addedToWorld(getWorld());
+        //}
+    }
 }
